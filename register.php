b10c233d8398cc664e55a09bce009d02c46e25cd
quality-ctce <nurd00915@gmail.com>
2024-11-17 00:42:10 +0800

    Update register.php

--- register.php
+++ register.php
@@ -1,9 +1,9 @@
 <?php
 require 'connect.php';
-$fullname = $_POST["fullname"];
-$username = $_POST["username"];
-$email = $_POST["email"];
-$password = $_POST["password"];
+$fullname = $_GET["fullname"];
+$username = $_GET["username"];
+$email = $_GET["email"];
+$password = $_GET["password"];
 
 $query_sql = "INSERT INTO tbl_users (fullname, username, email, password) VALUE ('$fullname', '$username', '$email', '$password')";
 
